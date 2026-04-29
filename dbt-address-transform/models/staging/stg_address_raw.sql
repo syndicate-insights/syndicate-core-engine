b7a3cf5f8ca7bfd0af6ffe640b08d8e4e72bb744
astom ahashie <astom.ahashie@lloydsbanking.com>
2026-04-29 12:56:45 +0100

    add dbt address transform

---
+++ dbt-address-transform/models/staging/stg_address_raw.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+/*
+    Reads raw address data from the GCS external table.
+    Filters out any files that have already been processed
+    (tracked in processed_files_metadata).
+*/
+
+select
+    address_id,
+    customer_id,
+    line1,
+    city,
+    postcode,
+    country,
+    _FILE_NAME as source_file
+from {{ source('gcs_raw', 'address_raw_external') }}
+where _FILE_NAME not in (
+    select file_name
+    from {{ ref('processed_files_metadata') }}
+    where file_name is not null
+)
